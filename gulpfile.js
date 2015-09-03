b9b71ee9c91d642fb840dffb40733d0b0ebcfe39
t ddddddd <webdev@cmme.org>
2015-09-02 23:34:18 -0700

    concat material.js into mdl-webcomponents.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var autoprefixer = require('gulp-autoprefixer');
 var babelify = require('babelify');
 var browserify = require('browserify');
+var buffer = require('gulp-buffer');
+var concat = require('gulp-concat');
 var cssInlineImages = require('gulp-css-inline-images');
 var csso = require('gulp-csso');
+var eventStream = require('event-stream');
+var exec = require('child_process').exec;
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var rename = require('gulp-rename');
@@ -25,16 +29,27 @@ var AUTOPREFIXER_BROWSERS = [
 var MDL_BASE = 'node_modules/material-design-lite/';
 
 
-gulp.task('default', ['cssjs'], function() {
-  return browserify({
+gulp.task('default', ['mdl-dist', 'cssjs'], function() {
+  var bundleStream = browserify({
       entries: './src/index.js',
       debug: true
     })
     .transform(babelify)
     .bundle()
-    .pipe(source('mdl-webcomponents.js'))
+    .pipe(source('mdl-bundle.js'))
+    .pipe(buffer())
+  ;
+
+  return eventStream.merge(gulp.src(MDL_BASE + 'dist/material.js'), bundleStream)
+    .pipe(concat('mdl-webcomponents.js'))
     .pipe(gulp.dest('./dist'))
   ;
+});
+
+gulp.task('mdl-dist', function(done) {
+  exec('gulp', {cwd: MDL_BASE}, function(err, stdout, stderr) {
+    done(err);
+  });
 });
 
 gulp.task('cssjs', function() {
