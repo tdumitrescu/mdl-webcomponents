3ad33b6622d6ed7d4fee9b2578e52871a00eccc3
t ddddddd <webdev@cmme.org>
2015-09-13 16:19:14 -0700

    pre-load Material Icons font

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+import CSS_MATERIAL_ICONS from './cssjs/material-icons.css';
 import registerButton from './material-button';
 import registerInput  from './material-input';
 
+// pre-load Material Icons font
+let headEl = document.head || document.getElementsByTagName('head')[0] || document.documentElement,
+  styleEl = document.createElement('style');
+headEl.appendChild(styleEl);
+styleEl.setAttribute('type', 'text/css');
+styleEl.textContent = CSS_MATERIAL_ICONS;
+
+// register components
 registerButton();
 registerInput();
