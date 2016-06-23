9155c8869049939d8112def10471cb82c5b70cfe
t ddddddd <webdev@cmme.org>
2016-06-22 20:07:10 -0700

    pull class definitions out of register functions

--- src/mdl-button.js
+++ src/mdl-button.js
@@ -4,27 +4,29 @@ import CSS_MATERIAL_ICONS from './cssjs/material-icons.css';
 import CSS_RIPPLE from './cssjs/ripple.css';
 import CSS_TYPOGRAPHY from './cssjs/typography.css';
 
-export default function() {
-  document.registerElement('mdl-button', class extends MDLComponent {
-    get MDL_SELECTORS() {
-      return ['button'];
-    }
+export class MDLButton extends MDLComponent {
+  get MDL_SELECTORS() {
+    return ['button'];
+  }
 
-    createDOM() {
-      var icon = this.getAttribute('icon'),
-          iconHTML = icon ? `<i class="material-icons">${icon}</i>` : '',
-          iconClass = icon ? 'icon' : 'accent',
-          className = `mdl-button mdl-js-button mdl-button--${iconClass}`,
-          buttonAttrs = this.hasAttribute('disabled') ? ' disabled' : '';
-      if (this.hasAttribute('raised')) {
-        className += " mdl-button--raised";
-      }
-      if (!this.hasAttribute('noink')) {
-        className += " mdl-js-ripple-effect";
-      }
-      this.createShadowRoot().innerHTML =
-        `<style>${CSS_BUTTON}${CSS_MATERIAL_ICONS}${CSS_RIPPLE}${CSS_TYPOGRAPHY}</style>` +
-        `<button class="${className}"${buttonAttrs}>${iconHTML}<content></content></button>`;
+  createDOM() {
+    var icon = this.getAttribute('icon'),
+        iconHTML = icon ? `<i class="material-icons">${icon}</i>` : '',
+        iconClass = icon ? 'icon' : 'accent',
+        className = `mdl-button mdl-js-button mdl-button--${iconClass}`,
+        buttonAttrs = this.hasAttribute('disabled') ? ' disabled' : '';
+    if (this.hasAttribute('raised')) {
+      className += " mdl-button--raised";
     }
-  });
+    if (!this.hasAttribute('noink')) {
+      className += " mdl-js-ripple-effect";
+    }
+    this.createShadowRoot().innerHTML =
+      `<style>${CSS_BUTTON}${CSS_MATERIAL_ICONS}${CSS_RIPPLE}${CSS_TYPOGRAPHY}</style>` +
+      `<button class="${className}"${buttonAttrs}>${iconHTML}<content></content></button>`;
+  }
+}
+
+export default function() {
+  document.registerElement('mdl-button', MDLButton);
 }
--- src/mdl-input.js
+++ src/mdl-input.js
@@ -4,59 +4,61 @@ import CSS_MATERIAL_ICONS from './cssjs/material-icons.css';
 import CSS_TEXTFIELD from './cssjs/textfield.css';
 import CSS_TYPOGRAPHY from './cssjs/typography.css';
 
-export default function() {
-  document.registerElement('mdl-input', class extends MDLComponent {
-    get MDL_SELECTORS() {
-      return ['.mdl-textfield', '.mdl-button--icon'];
-    }
+export class MDLInput extends MDLComponent {
+  get MDL_SELECTORS() {
+    return ['.mdl-textfield', '.mdl-button--icon'];
+  }
 
-    createDOM() {
-      var error = this.getAttribute('error'),
-          errorHTML = error ? `<span class="mdl-textfield__error">${error}</span>` : '',
+  createDOM() {
+    var error = this.getAttribute('error'),
+        errorHTML = error ? `<span class="mdl-textfield__error">${error}</span>` : '',
 
-          expandable = this.hasAttribute('expandable'),
-          expandableClass = expandable ? ' mdl-textfield--expandable' : '',
-          label = this.getAttribute('label') || 'Text...',
-          labelClass = this.hasAttribute('floating-label') ? ' mdl-textfield--floating-label' : '',
-          textfieldClasses = expandableClass + labelClass,
+        expandable = this.hasAttribute('expandable'),
+        expandableClass = expandable ? ' mdl-textfield--expandable' : '',
+        label = this.getAttribute('label') || 'Text...',
+        labelClass = this.hasAttribute('floating-label') ? ' mdl-textfield--floating-label' : '',
+        textfieldClasses = expandableClass + labelClass,
 
-          icon = this.getAttribute('icon'),
-          iconHTML = icon ? `<i class="material-icons">${icon}</i>` : '',
+        icon = this.getAttribute('icon'),
+        iconHTML = icon ? `<i class="material-icons">${icon}</i>` : '',
 
-          disabledHTML = this.hasAttribute('disabled') ? ' disabled' : '',
-          maxrows = this.getAttribute('maxrows'),
-          maxrowsHTML = maxrows ? ` maxrows="${maxrows}"` : '',
-          pattern = this.getAttribute('pattern'),
-          patternHTML = pattern ? ` pattern="${pattern}"` : '',
-          rows = this.getAttribute('rows'),
-          rowsHTML = rows ? ` rows="${rows}"` : '',
-          inputType = rows <= 1 ? 'input' : 'textarea',
-          inputAttrs = patternHTML + rowsHTML + maxrowsHTML + disabledHTML,
+        disabledHTML = this.hasAttribute('disabled') ? ' disabled' : '',
+        maxrows = this.getAttribute('maxrows'),
+        maxrowsHTML = maxrows ? ` maxrows="${maxrows}"` : '',
+        pattern = this.getAttribute('pattern'),
+        patternHTML = pattern ? ` pattern="${pattern}"` : '',
+        rows = this.getAttribute('rows'),
+        rowsHTML = rows ? ` rows="${rows}"` : '',
+        inputType = rows <= 1 ? 'input' : 'textarea',
+        inputAttrs = patternHTML + rowsHTML + maxrowsHTML + disabledHTML,
 
-          inputHTML =
-            `<${inputType} class="mdl-textfield__input" type="text" id="mdl-input1"${inputAttrs}></${inputType}>` +
-            `<label class="mdl-textfield__label" for="mdl-input1">${label}</label>`;
-
-      if (expandable) {
         inputHTML =
-          '<label class="mdl-button mdl-js-button mdl-button--icon" for="mdl-input1">' +
-            iconHTML +
-          '</label>' +
-          '<div class="mdl-textfield__expandable-holder">' +
-            inputHTML +
-          '</div>'
-      }
+          `<${inputType} class="mdl-textfield__input" type="text" id="mdl-input1"${inputAttrs}></${inputType}>` +
+          `<label class="mdl-textfield__label" for="mdl-input1">${label}</label>`;
 
-      this.createShadowRoot().innerHTML =
-        `<style>${CSS_BUTTON}${CSS_MATERIAL_ICONS}${CSS_TEXTFIELD}${CSS_TYPOGRAPHY}</style>` +
-        `<div class="mdl-textfield mdl-js-textfield${textfieldClasses}">` +
+    if (expandable) {
+      inputHTML =
+        '<label class="mdl-button mdl-js-button mdl-button--icon" for="mdl-input1">' +
+          iconHTML +
+        '</label>' +
+        '<div class="mdl-textfield__expandable-holder">' +
           inputHTML +
-          errorHTML +
-        '</div>';
+        '</div>'
     }
 
-    get value() {
-      return this.shadowRoot.querySelector('.mdl-textfield__input').value;
-    }
-  });
+    this.createShadowRoot().innerHTML =
+      `<style>${CSS_BUTTON}${CSS_MATERIAL_ICONS}${CSS_TEXTFIELD}${CSS_TYPOGRAPHY}</style>` +
+      `<div class="mdl-textfield mdl-js-textfield${textfieldClasses}">` +
+        inputHTML +
+        errorHTML +
+      '</div>';
+  }
+
+  get value() {
+    return this.shadowRoot.querySelector('.mdl-textfield__input').value;
+  }
+}
+
+export default function() {
+  document.registerElement('mdl-input', MDLInput);
 };
