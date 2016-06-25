bb23d8318b16217011314754bd621fbc5505adfe
t ddddddd <webdev@cmme.org>
2016-06-24 22:16:37 -0700

    finish converting input to Panel

--- src/mdl-input.js
+++ src/mdl-input.js
@@ -59,7 +59,14 @@ export class MDLInput extends MDLComponent {
       }, this.getAttribute('label') || 'Text...'),
     ];
 
-    // TODO check for expandable
+    if (this.isAttributeEnabled('expandable')) {
+      nodes = [
+        h('label.mdl-button.mdl-js-button.mdl-button--icon', {
+          htmlFor: 'mdl-input1',
+        }, this.iconNode()),
+        h('.mdl-textfield__expandable-holder', nodes),
+      ];
+    }
 
     nodes.push(this.errorNode());
 
@@ -69,51 +76,6 @@ export class MDLInput extends MDLComponent {
   errorNode() {
     const err = this.getAttribute('error');
     return err ? h('span.mdl-textfield__error', err) : '';
-  }
-
-  createDOM() {
-    var error = this.getAttribute('error'),
-        errorHTML = error ? `<span class="mdl-textfield__error">${error}</span>` : '',
-
-        expandable = this.hasAttribute('expandable'),
-        expandableClass = expandable ? ' mdl-textfield--expandable' : '',
-        label = this.getAttribute('label') || 'Text...',
-        labelClass = this.hasAttribute('floating-label') ? ' mdl-textfield--floating-label' : '',
-        textfieldClasses = expandableClass + labelClass,
-
-        icon = this.getAttribute('icon'),
-        iconHTML = icon ? `<i class="material-icons">${icon}</i>` : '',
-
-        disabledHTML = this.hasAttribute('disabled') ? ' disabled' : '',
-        maxrows = this.getAttribute('maxrows'),
-        maxrowsHTML = maxrows ? ` maxrows="${maxrows}"` : '',
-        pattern = this.getAttribute('pattern'),
-        patternHTML = pattern ? ` pattern="${pattern}"` : '',
-        rows = this.getAttribute('rows'),
-        rowsHTML = rows ? ` rows="${rows}"` : '',
-        inputType = rows <= 1 ? 'input' : 'textarea',
-        inputAttrs = patternHTML + rowsHTML + maxrowsHTML + disabledHTML,
-
-        inputHTML =
-          `<${inputType} class="mdl-textfield__input" type="text" id="mdl-input1"${inputAttrs}></${inputType}>` +
-          `<label class="mdl-textfield__label" for="mdl-input1">${label}</label>`;
-
-    if (expandable) {
-      inputHTML =
-        '<label class="mdl-button mdl-js-button mdl-button--icon" for="mdl-input1">' +
-          iconHTML +
-        '</label>' +
-        '<div class="mdl-textfield__expandable-holder">' +
-          inputHTML +
-        '</div>'
-    }
-
-    this.createShadowRoot().innerHTML =
-      `<style>${CSS_BUTTON}${CSS_MATERIAL_ICONS}${CSS_TEXTFIELD}${CSS_TYPOGRAPHY}</style>` +
-      `<div class="mdl-textfield mdl-js-textfield${textfieldClasses}">` +
-        inputHTML +
-        errorHTML +
-      '</div>';
   }
 
   get value() {
