66838006c14155b3fe4d551edb7139bfc0cd50de
t ddddddd <webdev@cmme.org>
2016-06-26 22:20:57 -0700

    convert textfield template to jade

--- src/mdl-textfield/index.js
+++ src/mdl-textfield/index.js
@@ -5,6 +5,7 @@ import CSS_BUTTON from 'material-design-lite/src/button/_button.scss';
 import CSS_MATERIAL_ICONS from '../common/material-icons.scss';
 import CSS_TEXTFIELD from 'material-design-lite/src/textfield/_textfield.scss';
 import CSS_TYPOGRAPHY from 'material-design-lite/src/typography/_typography.scss';
+import template from './index.jade';
 
 export class MDLTextfield extends MDLComponent {
   get MDL_SELECTORS() {
@@ -21,61 +22,22 @@ export class MDLTextfield extends MDLComponent {
         CSS_TYPOGRAPHY,
       ].join(''),
 
-      template: () => h('div', {
-        className: this.calcClassName(),
-      }, this.inputNodes()),
+      template,
 
       useShadowDom: true,
+
+      helpers: {
+        inputAttributes: () => {
+          const attributes = {};
+          for (const attr of ['maxrows', 'pattern', 'rows']) {
+            if (this.hasAttribute(attr)) {
+              attributes[attr] = this.getAttribute(attr);
+            }
+          }
+          return attributes;
+        },
+      },
     };
-  }
-
-  classList() {
-    return [
-      'mdl-textfield',
-      'mdl-js-textfield',
-      this.hasAttribute('expandable') ? 'mdl-textfield--expandable' : false,
-      this.hasAttribute('floating-label') ? 'mdl-textfield--floating-label' : false,
-    ];
-  }
-
-  inputNodes() {
-    const attributes = {};
-    for (const attr of ['maxrows', 'pattern', 'rows']) {
-      if (this.hasAttribute(attr)) {
-        attributes[attr] = this.getAttribute(attr);
-      }
-    }
-
-    let nodes = [
-      h(this.getAttribute('rows') <= 1 ? 'input' : 'textarea', {
-        attributes,
-        className: 'mdl-textfield__input',
-        disabled: this.isAttributeEnabled('disabled'),
-        id: 'mdl-input1',
-        type: 'text',
-      }),
-      h('label.mdl-textfield__label', {
-        htmlFor: 'mdl-input1',
-      }, this.getAttribute('label') || 'Text...'),
-    ];
-
-    if (this.isAttributeEnabled('expandable')) {
-      nodes = [
-        h('label.mdl-button.mdl-js-button.mdl-button--icon', {
-          htmlFor: 'mdl-input1',
-        }, this.iconNode()),
-        h('.mdl-textfield__expandable-holder', nodes),
-      ];
-    }
-
-    nodes.push(this.errorNode());
-
-    return nodes;
-  }
-
-  errorNode() {
-    const err = this.getAttribute('error');
-    return err ? h('span.mdl-textfield__error', err) : '';
   }
 
   get value() {
