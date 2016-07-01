5a5f22af2519c1909d0c78c81876a08cf9ae3373
t ddddddd <webdev@cmme.org>
2016-06-30 23:24:40 -0700

    button dynamic update tests

--- test/button.js
+++ test/button.js
@@ -14,4 +14,39 @@ describe('mdl-button', function() {
   it('proxies the disabled attribute', function() {
     expect(getMDLEl('dead-button').disabled).to.be.true;
   });
+
+  context('updated dynamically', function() {
+    var fixtureGarden;
+    var button, innerButton;
+
+    beforeEach(function(done) {
+      fixtureGarden = document.getElementById('fixture-garden');
+      fixtureGarden.innerHTML = '';
+      button = document.createElement('mdl-button');
+      fixtureGarden.appendChild(button);
+      window.requestAnimationFrame(function() {
+        innerButton = button.shadowRoot.querySelector('.mdl-button');
+        done();
+      });
+    });
+
+    it('can change the disabled attribute', function(done) {
+      expect(innerButton.disabled).not.to.be.ok;
+      button.setAttribute('disabled', true);
+      window.requestAnimationFrame(function() {
+        expect(innerButton.disabled).to.be.true;
+        done();
+      });
+    });
+
+    it('can add/change icons', function(done) {
+      expect(button.shadowRoot.querySelector('.material-icons')).to.be.null;
+      button.setAttribute('icon', 'build');
+      window.requestAnimationFrame(function() {
+        var icon = button.shadowRoot.querySelector('.material-icons');
+        expect(icon.textContent).to.equal('build');
+        done();
+      });
+    });
+  });
 });
