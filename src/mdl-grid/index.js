eb2760eadab329b9280730359c717b8f560e4cb1
t ddddddd <webdev@cmme.org>
2016-09-10 23:32:56 -0700

    grid implementation

--- src/mdl-grid/index.js
+++ src/mdl-grid/index.js
@@ -2,12 +2,33 @@ import { MDLComponent } from '../component';
 import template from './index.jade';
 import css from './index.scss';
 
+let cellTargetID = 1;
+function generateTarget(cellEl) {
+  const target = `mdl-cell-${cellTargetID++}`;
+  cellEl.setAttribute('target', target);
+  return target;
+}
+
 export class MDLGrid extends MDLComponent {
   get config() {
     return {
       css,
       template,
       useShadowDom: true,
+      helpers: {
+        cells: () => {
+          const cells = [];
+          const cellEls = this.querySelectorAll('mdl-cell');
+          for (let ci = 0; ci < cellEls.length; ci++) {
+            const cellEl = cellEls[ci];
+            cells.push({
+              cols: cellEl.getAttribute('cols'),
+              target: cellEl.getAttribute('target') || generateTarget(cellEl),
+            });
+          }
+          return cells;
+        },
+      },
     };
   }
 }
